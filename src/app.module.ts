aeefb1f8ee62764204678633cb5f948600aca7bf
Kenzo Sato <kenzohideaky@gmail.com>
2026-01-04 18:33:44 -0300

    fix: removed boilerplate nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './adapters/driven/persistance/prisma.module';
 import { CustomerModule } from './customer.module';
 
 @Module({
   imports: [PrismaModule, CustomerModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
